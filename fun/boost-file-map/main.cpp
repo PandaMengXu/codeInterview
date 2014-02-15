68ce09017b28b6b94fc74e7bd6099d275228e84a
Meng Xu <mengxu@cis.upenn.edu>
2014-02-15 18:33:18 -0500

    workable version of boost:iostreams::mapped_file! finish the boost file read/write! Very useful and efficient liberary! :)

--- fun/boost-file-map/main.cpp
+++ fun/boost-file-map/main.cpp
@@ -1,24 +1,24 @@
 #include <boost/iostreams/device/mapped_file.hpp>
 #include <iostream>
 
+int numberOfElements = 1000;
+int numberOfBytes = numberOfElements*sizeof(int);
 
-int main(){
+void read_write_unidirection_example()
+{
     boost::iostreams::mapped_file_source file_source;
     boost::iostreams::mapped_file_sink file_sink;
     boost::iostreams::mapped_file_params param_sink;
-//    boost::iostreams::stream<boost::iostreams::mapped_file_sinka>    out;
-    int numberOfElements = 1000;
-    int numberOfBytes = numberOfElements*sizeof(int);
 
     file_source.open("filename.raw", numberOfBytes);
-    param_sink.path = "result.output";
+    param_sink.path = "result-unidirection.output";
     param_sink.offset = 0;
-    param_sink.new_file_size = numberOfBytes;
+    param_sink.new_file_size = numberOfBytes; /*This has to be the size of bytes to write!*/
     file_sink.open(param_sink);
-//    out.open(file_sink(param_sink));
 
     // Check if file was successfully opened
-    if(file_source.is_open() && file_sink.is_open()) {
+    if(file_source.is_open() && file_sink.is_open()) 
+    {
         // Get pointer to the data
         int * data = (int *)file_source.data();
  
@@ -35,6 +35,47 @@ int main(){
         file_sink.close();
 //        out.close();
     } else {
-        std::cout << "could not map the file filename.raw or result.output" << std::endl;
+        std::cout << "could not map the file filename.raw or result-unidirection.output" << std::endl;
     }
+}
+
+void read_write_bidirection_example()
+{
+    namespace bio = boost::iostreams;
+    bio::mapped_file_source file_source;
+    bio::mapped_file file;
+    bio::mapped_file_params param;
+
+    file_source.open("result-unidirection.output", numberOfBytes);
+    param.path = "result-bidirection.output";
+    param.offset = 0;
+    param.mode = std::ios_base::in | std::ios_base::out;
+    param.new_file_size = numberOfBytes;
+    file.open(param);
+    
+    if(file_source.is_open() && file.is_open())
+    {
+        /* Get the start of the memory address where the input file is mapped*/
+        int * data_src = (int *) file_source.data(); 
+        int * data_out = (int *) file.const_data();
+
+        memcpy(file.data(), data_src, numberOfBytes);
+
+        /* Read data just wrote*/
+        for(int i = 0; i < numberOfElements; i++)
+        {
+            std::cout << data_out[i] << " ";
+        }
+
+        file_source.close();
+        file.close();
+    } else{
+        std::cout << "could not map the file filename.raw or result-bidirection.output" << std::endl;
+    }
+}
+
+int main(){
+    read_write_unidirection_example();
+    std::cout << "\n=====================================================\n" << std::endl;
+    read_write_bidirection_example();
 }
