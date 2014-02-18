17f426231cf3233c990a1a758cb2b3d1873f0ac1
pennpanda <pennpanda@pennpanda.(none)>
2014-02-17 21:52:51 -0500

    m

--- fun/boost-file-map/main.cpp
+++ fun/boost-file-map/main.cpp
@@ -1,81 +1,52 @@
 #include <boost/iostreams/device/mapped_file.hpp>
 #include <iostream>
-
-int numberOfElements = 1000;
+#include <sstream>
+#include <string>
+int numberOfElements = 1000000;
 int numberOfBytes = numberOfElements*sizeof(int);
 
 void read_write_unidirection_example()
 {
-    boost::iostreams::mapped_file_source file_source;
-    boost::iostreams::mapped_file_sink file_sink;
-    boost::iostreams::mapped_file_params param_sink;
+       
+        boost::iostreams::mapped_file_sink file_sink;
+        boost::iostreams::mapped_file_params param_sink;
+        uint16_t offset = 0;
+        uint16_t page;
+        param_sink.path = "result-unidirection.output";
+        param_sink.offset = 0;
+        param_sink.new_file_size = file_sink.alignment()*4; /*This has to be the size of bytes to write!*/
+        file_sink.open(param_sink);
+        std::cout<<param_sink.new_file_size<<std::endl;
+        if( ! file_sink.is_open()) 
+                return;
 
-    file_source.open("filename.raw", numberOfBytes);
-    param_sink.path = "result-unidirection.output";
-    param_sink.offset = 0;
-    param_sink.new_file_size = numberOfBytes; /*This has to be the size of bytes to write!*/
-    file_sink.open(param_sink);
-
-    // Check if file was successfully opened
-    if(file_source.is_open() && file_sink.is_open()) 
-    {
-        // Get pointer to the data
-        int * data = (int *)file_source.data();
- 
-        memcpy(file_sink.data(), data, numberOfBytes);
-        // Do something with the data
-        for(int i = 0; i < numberOfElements; i++)
-        {
-            //file_sink.write((data+i), sizeof(int));
-            std::cout << data[i] << " ";
+        for(int i=0; i<4000; i++)
+        {   
+                std::stringstream ss;
+                ss<<"LOG ********"<<i<<"-"<<offset<<"*********\n";
+                int slen = strlen(ss.str().c_str()); 
+                std::cout<<ss.str()<<offset<<"+"<<slen<<" "<<(offset + slen > param_sink.new_file_size)<<std::endl; 
+                if(offset + slen > param_sink.new_file_size)
+                {
+                        std::cout<<"Exceed memory limit @"<<i<<" "<<offset<<"+"<<slen<<std::endl;
+                        file_sink.close();
+                        page += 1;
+                        offset = 0;
+                        param_sink.offset = param_sink.new_file_size;
+                        param_sink.new_file_size *= 2;
+                        std::cout<<"file.offset: "<<param_sink.offset<<" file.size: "<<param_sink.new_file_size<<std::endl;
+                        file_sink.open(param_sink);
+                        std::cout<<"new mapped_file created!"<<std::endl;
+                }
+                 
+                memcpy(file_sink.data()+offset, ss.str().c_str(), slen);
+                offset += slen;
         }
- 
-        // Remember to unmap the file
-        file_source.close();
         file_sink.close();
-//        out.close();
-    } else {
-        std::cout << "could not map the file filename.raw or result-unidirection.output" << std::endl;
-    }
 }
 
-void read_write_bidirection_example()
-{
-    namespace bio = boost::iostreams;
-    bio::mapped_file_source file_source;
-    bio::mapped_file file;
-    bio::mapped_file_params param;
 
-    file_source.open("result-unidirection.output", numberOfBytes);
-    param.path = "result-bidirection.output";
-    param.offset = 0;
-    param.mode = std::ios_base::in | std::ios_base::out;
-    param.new_file_size = numberOfBytes;
-    file.open(param);
-    
-    if(file_source.is_open() && file.is_open())
-    {
-        /* Get the start of the memory address where the input file is mapped*/
-        int * data_src = (int *) file_source.data(); 
-        int * data_out = (int *) file.const_data();
+int main(int argc, char * argv[]){
 
-        memcpy(file.data(), data_src, numberOfBytes);
-
-        /* Read data just wrote*/
-        for(int i = 0; i < numberOfElements; i++)
-        {
-            std::cout << data_out[i] << " ";
-        }
-
-        file_source.close();
-        file.close();
-    } else{
-        std::cout << "could not map the file filename.raw or result-bidirection.output" << std::endl;
-    }
-}
-
-int main(){
-    read_write_unidirection_example();
-    std::cout << "\n=====================================================\n" << std::endl;
-    read_write_bidirection_example();
+        read_write_unidirection_example();
 }
