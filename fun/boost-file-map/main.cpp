5c0373f53aba82faf14258367c312f6c00d1acc5
pennpanda <pennpanda@pennpanda.(none)>
2014-02-17 23:07:32 -0500

    no bus erro

--- fun/boost-file-map/main.cpp
+++ fun/boost-file-map/main.cpp
@@ -7,14 +7,15 @@ int numberOfBytes = numberOfElements*sizeof(int);
 
 void read_write_unidirection_example()
 {
-       
+
         boost::iostreams::mapped_file_sink file_sink;
         boost::iostreams::mapped_file_params param_sink;
-        uint16_t offset = 0;
-        uint16_t page;
-        param_sink.path = "result-unidirection.output";
+        int offset = 0;
+        int page;
+        bool restart = true;
+        param_sink.path = "result.log";
         param_sink.offset = 0;
-        param_sink.new_file_size = file_sink.alignment()*4; /*This has to be the size of bytes to write!*/
+        param_sink.new_file_size = file_sink.alignment(); /*This has to be the size of bytes to write!*/
         file_sink.open(param_sink);
         std::cout<<param_sink.new_file_size<<std::endl;
         if( ! file_sink.is_open()) 
@@ -25,21 +26,32 @@ void read_write_unidirection_example()
                 std::stringstream ss;
                 ss<<"LOG ********"<<i<<"-"<<offset<<"*********\n";
                 int slen = strlen(ss.str().c_str()); 
-                std::cout<<ss.str()<<offset<<"+"<<slen<<" "<<(offset + slen > param_sink.new_file_size)<<std::endl; 
-                if(offset + slen > param_sink.new_file_size)
+                //                std::cout<<i<<" "<<offset<<"+"<<slen<<"="<<offset+slen<<" "<<(offset + slen > param_sink.new_file_size)<<"|"; 
+                if(offset + slen > file_sink.alignment())
                 {
                         std::cout<<"Exceed memory limit @"<<i<<" "<<offset<<"+"<<slen<<std::endl;
                         file_sink.close();
                         page += 1;
                         offset = 0;
                         param_sink.offset = param_sink.new_file_size;
-                        param_sink.new_file_size *= 2;
+                        param_sink.new_file_size += file_sink.alignment();
                         std::cout<<"file.offset: "<<param_sink.offset<<" file.size: "<<param_sink.new_file_size<<std::endl;
                         file_sink.open(param_sink);
+                        if(!file_sink.is_open()) return;
                         std::cout<<"new mapped_file created!"<<std::endl;
+                        restart = true;
                 }
-                 
-                memcpy(file_sink.data()+offset, ss.str().c_str(), slen);
+                char * pointer= file_sink.data()+offset;
+                if(restart) 
+                {
+                        std::cout<<(void*)file_sink.data()<<"+"<<offset<<" = "<<(void*)pointer<<std::endl; 
+
+                }
+                memcpy(pointer, ss.str().c_str(), slen);
+                if(restart){
+                        std::cout<<"hehe"<<std::endl;
+                        restart = false;
+                }
                 offset += slen;
         }
         file_sink.close();
