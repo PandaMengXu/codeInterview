a895c3b29d7fa7f491ef11171c0daa1030d9fabd
Meng Xu <mengxu@cis.upenn.edu>
2014-02-17 19:39:51 -0500

    runnable version of aio_write(1), but the buffer_out may be overwritten if it has not been written to the file before it's overwriten by memcpy(buffer_out, buffer, buffer_size)

--- fun/aio_write/aio_write.cpp
+++ fun/aio_write/aio_write.cpp
@@ -7,6 +7,9 @@
 #include <errno.h>
 using namespace std;
 
+#define true 1
+#define false 0
+
 int main()
 {
     int i = 0;
@@ -14,16 +17,17 @@ int main()
     int slen = 0;
     char* buffer, *buffer_out;
     int buffer_size = 30;
+    int is_first_write = true;
 
     struct aiocb aio_param;
-    int fd_out = open("result.log", O_APPEND);    
+    int fd_out = open("result.log", O_APPEND | O_RDWR);    
 
     buffer = (char*) malloc(buffer_size);
     buffer_out = (char*) malloc(buffer_size);
     memset(buffer, 0, buffer_size);
     memset(buffer_out, 0, buffer_size);
 
-    for( i = 0; i < 10000; i++)
+    for( i = 0; i < 10; i++)
     {
         stringstream ss;
         ss << "LOG *****" << i << "-" << "********\n";
@@ -33,8 +37,10 @@ int main()
         if(offset + slen > buffer_size)
         {
         //write the current buffer content (< 4K) into the file
+            memset(&aio_param, 0, sizeof(aio_param));
             aio_param.aio_fildes = fd_out;
-            //aio_param.aio_offset = fd_offset;
+            aio_param.aio_offset = fd_offset;
+            while( aio_error(&aio_param) != 0 && is_first_write == false ) {cout << "wait for the previous one finish" <<endl;}
             aio_param.aio_buf = memcpy(buffer_out, buffer, buffer_size); //the buffer_out has to be written before it's copied to again!
             aio_param.aio_nbytes = offset;
             //aio_offset = ? should be the end of file for O_APPEND mode
@@ -45,16 +51,20 @@ int main()
                 cout<<"errno:" << errno <<endl;
                 return 1;
             }else{
+                is_first_write = false;
                 fd_offset += offset;
+                cout<<"aio_write()" << offset << " bytes succeed at file offset " << fd_offset<<endl;
+                offset = 0;
+             //   sleep(1);
+            //clean the buffer
+                memset(buffer, 0, buffer_size);
             }
-            cout<<"aio_write()" << offset << " bytes succeed!"<<endl;
-        //clean the buffer
-            memset(buffer, 0, buffer_size);
         }
         //write the current string to the buffer 
         memcpy(buffer + offset, ss.str().c_str(), slen);
         offset += slen;
     }
+    close(fd_out);
     return 0;
 
 }
